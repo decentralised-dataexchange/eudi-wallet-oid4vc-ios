80edda91fab0c030e6319bb41b60fb170c038285
Joseph Milan <milan@igrant.io>
2024-07-05 16:34:24 +0530

    Fix: Error handling in verification Send VP token

--- Sources/eudiWalletOidcIos/Service/VerificationServiceProtocol.swift
+++ Sources/eudiWalletOidcIos/Service/VerificationServiceProtocol.swift
@@ -24,7 +24,7 @@ protocol VerificationServiceProtocol {
         secureKey: SecureKeyData,
         presentationRequest: PresentationRequest?,
         credentialsList: [String]?
-    ) async -> Data?
+    ) async -> WrappedVerificationResponse?
 
     /**
      Processes an authorization request and extracts a PresentationRequest object asynchronously.
