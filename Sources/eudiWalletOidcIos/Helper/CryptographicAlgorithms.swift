88e2789a53a1c88d9b219bfb241e9e13b9d34bef
Joseph Milan <milan@igrant.io>
2024-07-25 19:09:00 +0530

    Fix #33: Support for EdDSA cryptographic suit

---
+++ Sources/eudiWalletOidcIos/Helper/CryptographicAlgorithms.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Milan on 25/07/24.
+//
+
+import Foundation
+
+public enum CryptographicAlgorithms: String {
+    case ES256 = "ES256"
+    case EdDSA = "EdDSA"
+}
