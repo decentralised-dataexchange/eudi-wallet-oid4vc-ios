d7aec391f436b436aee02101855f4cd3bd86e48a
Josep Milan K.A <milan@igrant.io>
2024-08-30 17:27:02 +0530

    Fix #42: Support for client metadata during verification

--- Sources/eudiWalletOidcIos/Model/PresentationRequest.swift
+++ Sources/eudiWalletOidcIos/Model/PresentationRequest.swift
@@ -4,14 +4,12 @@
 //
 //  Created by Mumthasir mohammed on 18/03/24.
 //
-
 import Foundation
-
 public struct PresentationRequest: Codable {
     public var state, clientId, redirectUri, responseType, responseMode, scope, nonce, requestUri: String?
     public var responseUri: String?
     public var presentationDefinition: String?
-
+    public var clientMetaData: String?
     enum CodingKeys: String, CodingKey {
         case state = "state"
         case clientId = "client_id"
@@ -23,9 +21,9 @@ public struct PresentationRequest: Codable {
         case nonce = "nonce"
         case requestUri = "request_uri"
         case presentationDefinition = "presentation_definition"
+        case clientMetaData = "client_metadata"
     }
-
-    public init(state: String?, clientId: String?, redirectUri: String?, responseUri: String?, responseType: String?, responseMode: String?, scope: String?, nonce: String?, requestUri: String?, presentationDefinition: String?) {
+    public init(state: String?, clientId: String?, redirectUri: String?, responseUri: String?, responseType: String?, responseMode: String?, scope: String?, nonce: String?, requestUri: String?, presentationDefinition: String?, clientMetaData:  String?) {
         self.state = state
         self.clientId = clientId
         self.redirectUri = redirectUri
@@ -36,6 +34,7 @@ public struct PresentationRequest: Codable {
         self.nonce = nonce
         self.requestUri = requestUri
         self.presentationDefinition = presentationDefinition
+        self.clientMetaData = clientMetaData
     }
     
     public init(from decoder: Decoder) throws {
@@ -57,5 +56,34 @@ public struct PresentationRequest: Codable {
         } else {
             presentationDefinition = nil
         }
+        if let clientMetaDataModel = try? container.decode(ClientMetaData.self, forKey: . clientMetaData) {
+            clientMetaData = clientMetaDataModel.toJSONString()
+        }
+    }
+}
+public struct ClientMetaData: Codable {
+    public var clientName, coverUri, description, location, logoUri: String?
+    enum CodingKeys: String, CodingKey {
+        case clientName = "client_name"
+        case coverUri = "cover_uri"
+        case description = "description"
+        case location = "location"
+        case logoUri = "logo_uri"
+    }
+    public init(clientName: String?, coverUri: String?, description: String?, location: String?, logoUri: String?) {
+        self.clientName = clientName
+        self.coverUri = coverUri
+        self.description = description
+        self.location = location
+        self.logoUri = logoUri
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        clientName = try container.decodeIfPresent(String.self, forKey: .clientName)
+        coverUri = try container.decodeIfPresent(String.self, forKey: .coverUri)
+        description = try container.decodeIfPresent(String.self, forKey: .description)
+        location = try container.decodeIfPresent(String.self, forKey: .location)
+        logoUri = try container.decodeIfPresent(String.self, forKey: .logoUri)
     }
 }
