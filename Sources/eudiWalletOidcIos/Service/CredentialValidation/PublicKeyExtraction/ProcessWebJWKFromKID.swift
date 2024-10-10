070ceec8613627428c792a221dff12beb48ef6c5
Josep Milan K.A <milan@igrant.io>
2024-10-10 14:40:39 +0530

    Fix: Support for DID:WEB and DID:JWK

---
+++ Sources/eudiWalletOidcIos/Service/CredentialValidation/PublicKeyExtraction/ProcessWebJWKFromKID.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//
+//
+//  Created by oem on 10/10/24.
+//
+
+import Foundation
+
+class ProcessWebJWKFromKID {
+    static func fetchDIDDocument(did: String) async throws -> [String: Any]? {
+        guard did.hasPrefix("did:web:") else { return nil }
+        
+        let didWithoutPrefix = did.replacingOccurrences(of: "did:web:", with: "")
+        let didParts = didWithoutPrefix.split(separator: ":")
+        
+        guard didParts.count > 1 else { return nil }
+        
+        let host = didParts[0]
+        let path = didParts[1].split(separator: "#").first ?? ""
+        let didDocURLString = "https://\(host)/\(path)/did.json"
+        
+        guard let didDocURL = URL(string: didDocURLString) else { return nil }
+        
+        let (data, response) = try await URLSession.shared.data(from: didDocURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            return nil
+        }
+        
+        let didDoc = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        
+        return didDoc
+    }
+}
