889c97d83c1f61618102b18c11de533f815e61a5
Josep Milan K.A <milan@igrant.io>
2024-10-10 14:40:39 +0530

    Fix: Support for DID:WEB and DID:JWK

---
+++ Sources/eudiWalletOidcIos/Service/CredentialValidation/PublicKeyExtraction/ProcessJWKFromJwksUri.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//
+//
+//  Created by oem on 10/10/24.
+//
+
+import Foundation
+
+class ProcessJWKFromJwksUri {
+    
+    static func processJWKFromJwksURI2(kid: String?, jwksURI: String?) async -> [String: Any] {
+        guard let jwksURI = jwksURI else {return [:]}
+        return await fetchJwkData(kid: kid, jwksUri: jwksURI)
+    }
+    
+    static func fetchJwkData(kid: String?, jwksUri: String)async -> [String: Any] {
+        guard let url = URL(string: jwksUri) else {
+            return [:]
+        }
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { return [:]}
+            guard let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let keys = jsonObject["keys"] as? [[String: Any]] else { return [:]}
+            
+            var jwkKey: [String: Any]? = keys.first { $0["use"] as? String == "sig" }
+            
+            if jwkKey == nil, let kid = kid {
+                jwkKey = keys.first { $0["kid"] as? String == kid }
+            }
+            return jwkKey ?? [:]
+            
+        } catch {
+            print("error")
+        }
+        return [:]
+    }
+}
